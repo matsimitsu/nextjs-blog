4a1b9ff74b2ab3dcb3c20d0f14a00a661a95885d
Robert Beekman <robertbeekman@matsimitsu.nl>
2021-07-10 11:30:43 +0200

    Attempt to get SHA from env

--- appsignal.js
+++ appsignal.js
@@ -3,4 +3,5 @@ import Appsignal from "@appsignal/javascript";
 export default new Appsignal({
   key: "840e135a-2edf-47fe-9e5e-fa46aabf0c2c",
   uri: "https://error-tracker.staging.lol/collect",
+  revision: process.env.VERCEL_GIT_COMMIT_SHA || process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA
 });
